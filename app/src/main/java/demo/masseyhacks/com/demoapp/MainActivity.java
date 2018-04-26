66d4ed16d01481daff7e83fc7dfdd05b7e8421de
haris <haris@MacbookPro.local>
2018-04-25 20:23:23 -0400

    Add demo elements

--- app/src/main/java/demo/masseyhacks/com/demoapp/MainActivity.java
+++ app/src/main/java/demo/masseyhacks/com/demoapp/MainActivity.java
@@ -1,13 +1,72 @@
 package demo.masseyhacks.com.demoapp;
 
+import android.app.Dialog;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button)findViewById(R.id.button);
+
+        //make the button do something
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Button Clicked",Toast.LENGTH_SHORT).show();
+
+                //create a new popup
+                showPopupMessage("Hello this is a popup");
+            }
+        });
     }
+
+    public void showPopupMessage(String message){
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog);
+
+        TextView textView = dialog.findViewById(R.id.textView);
+        textView.setText(message);
+
+        Button button = dialog.findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendNotification();
+            }
+        });
+
+
+        dialog.show();
+    }
+
+    public void sendNotification(){
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("My notification")
+                .setContentText("Much longer text that cannot fit one line...")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("Much longer text that cannot fit one line..."))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        // Gets an instance of the NotificationManager service//
+
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify((int)System.currentTimeMillis(),mBuilder.build());
+    }
+
+
 }
